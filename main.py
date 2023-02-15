eab528a91b7f7f5a9e1e1d1c957275cdbf3eb118
Seth Christmus <seth.christmus@fdmgroup.com>
2023-02-15 14:49:31 -0500

    Add remaining game logic (2 more choices)

--- main.py
+++ main.py
@@ -30,6 +30,21 @@ answer1 = input("You awaken on a deserted island with a piece of parchment in yo
 
 if answer1 == 'Y' or answer1 == 'y':
     print("\nYou open the parchment.")
-    answer2 = input("It's a treasure map! There are two Xs on the map - one to the northeast and one to the northwest. Which way do you go? Type 1 for northeast, 2 for northwest ")
+    answer2 = input("It's a treasure map! There are two Xs on the map - one to the northeast and one to the "
+                    "northwest. Which way do you go? Type 1 for northeast, 2 for northwest ")
+    if answer2 == '2':
+        print("\nYou begin walking to the northwest.")
+        answer3 = input("You come to a river. You can swim across or use the old wooden bridge. What do you do? Type "
+                        "swim or bridge ")
+        if answer3 == 'bridge':
+            print("\nYou cross the bridge quickly without incident.")
+            print("You arrive at the X on the map and begin to dig with your hands. Soon enough, you dig up a heavy "
+                  "chest and open it. It's full of TREASURE! YOU WIN!")
+        else:
+            print("\nYou try to swim across, but the river is full of alligators. Uh-oh. GAME OVER")
+    else:
+        print("\nYou head northeast. Shortly after beginning your journey, you fall into a well-disguised hole full of "
+              "snakes. GAME OVER")
 else:
-    print("You decide not to open the parchment. You spend the rest of your brief life waiting for help to come to the island. GAME OVER")
+    print("\nYou decide not to open the parchment. You spend the rest of your brief life waiting for help to come to "
+          "the island. GAME OVER")
